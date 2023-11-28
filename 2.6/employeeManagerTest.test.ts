dc02e6cff4230e5ac21b70f1df38fe84175eb13c
Markiesia Scott <shunte816@gmail.com>
2023-11-27 18:13:34 -0600

    Unit2.6 Exercise Complete

--- 2.6/employeeManagerTest.test.ts
+++ 2.6/employeeManagerTest.test.ts
@@ -9,17 +9,33 @@ import {Builder,By, Capabilities, until, WebDriver, } from "selenium-webdriver";
       driver: WebDriver;
       url: string = "https://devmountain-qa.github.io/employee-manager/1.2_Version/index.html";
         //FILL OUT LOCATORS CONSTRUCTOR AND METHODS IN ORDER TO PASS THE TEST
-  }
+        header: By = By.css('.titleText');
+        addEmployee: By = By.name('addEmployee'); 
+        newEmployee: By = By.name('employee11'); 
+        nameInput: By = By.name('nameEntry'); 
+        phoneInput: By = By.name('phoneEntry'); 
+        titleInput: By = By.name('titleEntry'); 
+
+        //Constructor
+        constructor(driver:WebDriver) {
+            this.driver = driver;
+        };
+        //navigate method
+        async navigate() {
+            await this.driver.get(this.url);
+            await this.driver.wait(until.elementLocated(this.header));
+        };
+    }
+    const emPage = new employeePage(driver);
 
   describe("Employee Manger Test", () => {
       beforeEach(async () => {
-          await employeePage.navigate();
+          await emPage.navigate();
       })
       afterAll(async () => {
           await driver.quit()
       })
       test("adding an employee", async () => {
-          await driver.wait(until.elementLocated(emPage.header))
           await driver.findElement(emPage.addEmployee).click()
           await driver.findElement(emPage.newEmployee).click()
           await driver.findElement(emPage.nameInput).click()
@@ -31,6 +47,6 @@ import {Builder,By, Capabilities, until, WebDriver, } from "selenium-webdriver";
           await driver.findElement(emPage.titleInput).click()
           await driver.findElement(emPage.titleInput).clear()
           await driver.findElement(emPage.titleInput).sendKeys("Change this")
-  })
-
+  });
+    })
   /* this is a commment */
